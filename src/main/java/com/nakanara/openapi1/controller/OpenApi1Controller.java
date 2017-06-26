bc73fec752b3003f9f44c098246723a1fe9a9ab3
nakanara <nakanara@naver.com>
2017-06-26 23:54:46 +0900

    테스트

--- src/main/java/com/nakanara/openapi1/controller/OpenApi1Controller.java
+++ src/main/java/com/nakanara/openapi1/controller/OpenApi1Controller.java
@@ -1,5 +1,7 @@
 package com.nakanara.openapi1.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -10,8 +12,11 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class OpenApi1Controller {
 
-    @RequestMapping("/welcome")
+    private Logger logger = LoggerFactory.getLogger(OpenApi1Controller.class);
+
+    @RequestMapping("/welcome.do")
     public ModelAndView helloWorld() {
+        logger.info("INFO~~~~Controller");
         String message = "<br><div style='text-align:center;'>"
                 + "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from CrunchifyHelloWorld.java **********</div><br><br>";
         return new ModelAndView("welcome", "message", message);
