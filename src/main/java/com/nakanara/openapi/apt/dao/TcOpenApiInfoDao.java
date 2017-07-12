7eb1cbfbb10ce7856dc5f2092c79893fb3a496fb
nakanara <nakanara@naver.com>
2017-07-12 18:04:05 +0900

    신규 테이블 추가 tc openapi info

---
+++ src/main/java/com/nakanara/openapi/apt/dao/TcOpenApiInfoDao.java
@@ -0,0 +1,27 @@
+package com.nakanara.openapi.apt.dao;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Created by steg on 2017-07-12.
+ */
+@Entity
+@Data
+public class TcOpenApiInfoDao {
+
+    @Id
+    @Getter @Setter private String api_seq;
+    @Getter @Setter private String api_name;
+    @Getter @Setter private String api_desc;
+
+    @Getter @Setter private String api_url_dev;
+    @Getter @Setter private String api_url_op;
+    @Getter @Setter private String api_reg_dttm;
+    @Getter @Setter private String api_mod_dttm;
+
+}
